3ff74b11fe3dbf27f2c1accab682ff62c62c80d9
Stefan Minkov <50842434+aRowOf5@users.noreply.github.com>
2025-07-04 16:12:01 +0300

    Add files via upload

---
+++ Moderator/Moderator.cpp
@@ -0,0 +1,87 @@
+﻿#include "Moderator.h"
+
+/**
+ * @brief Constructor that creates a new Moderator object with the specified parameters.
+ *
+ * @param firstName Moderator's first name.
+ * @param lastName Moderator's last name.
+ * @param nickname Moderator's nickname.
+ * @param password Moderator's password.
+ */
+Moderator::Moderator(const std::string& firstName, const std::string& lastName, const std::string& nickname, const std::string& password) :
+    User(firstName, lastName, nickname, password){
+    setPermissionRole(Permission::MOD);
+}
+
+/**
+ * @brief Default constructor.
+ */
+Moderator::Moderator() : User() {
+	setPermissionRole(Permission::MOD);
+}
+
+/**
+ * @brief Clones the Moderator object.
+ *
+ * @return Pointer to the new User object (in this case Moderator).
+ */
+User* Moderator::clone() const {
+    return new Moderator(*this);
+}
+
+/**
+ * @brief Saves moderator data to a file.
+ *
+ * @param of The output file stream.
+ */
+void Moderator::writeToFile(std::ofstream& of) const {
+	of.write(reinterpret_cast<const char*>(&role), sizeof(Permission));
+
+	unsigned int size = firstName.size();
+	of.write(reinterpret_cast<const char*>(&size), sizeof(size));
+	of.write((const char*)&firstName[0], size);
+
+	size = lastName.size();
+	of.write(reinterpret_cast<const char*>(&size), sizeof(size));
+	of.write((const char*)&lastName[0], size);
+
+	size = nickname.size();
+	of.write(reinterpret_cast<const char*>(&size), sizeof(size));
+	of.write((const char*)&nickname[0], size);
+
+	size = password.size();
+	of.write(reinterpret_cast<const char*>(&size), sizeof(size));
+	of.write((const char*)&password[0], size);
+
+	of.write(reinterpret_cast<const char*>(&id), sizeof(id));
+	of.write(reinterpret_cast<const char*>(&points), sizeof(points));
+	of.write(reinterpret_cast<const char*>(&role), sizeof(Permission));
+}
+
+/**
+ * @brief Reads moderator data from a file.
+ *
+ * @param iff The input file stream.
+ */
+void Moderator::readFromFile(std::ifstream& iff) {
+	unsigned int size = 0;
+	iff.read(reinterpret_cast<char*>(&size), sizeof(size));
+	firstName.resize(size);
+	iff.read((char*)&firstName[0], size);
+
+	iff.read(reinterpret_cast<char*>(&size), sizeof(size));
+	lastName.resize(size);
+	iff.read((char*)&lastName[0], size);
+
+	iff.read(reinterpret_cast<char*>(&size), sizeof(size));
+	nickname.resize(size);
+	iff.read((char*)&nickname[0], size);
+
+	iff.read(reinterpret_cast<char*>(&size), sizeof(size));
+	password.resize(size);
+	iff.read((char*)&password[0], size);
+
+	iff.read(reinterpret_cast<char*>(&id), sizeof(id));
+	iff.read(reinterpret_cast<char*>(&points), sizeof(points));
+	iff.read(reinterpret_cast<char*>(&role), sizeof(Permission));
+}
---
+++ Moderator/Moderator.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "User.h"
+
+/**
+ * @brief The Moderator class inherits the User class and adds specific functionality for moderators.
+ */
+class Moderator : public User {
+public:
+    /**
+     * @brief Constructor that creates a new Moderator object with the specified parameters.
+     *
+     * @param firstName Moderator's first name.
+     * @param lastName Moderator's last name.
+     * @param nickname Moderator's nickname.
+     * @param password Moderator's password.
+     */
+    Moderator(const std::string& firstName, const std::string& lastName, const std::string& nickname, const std::string& password);
+
+    /**
+     * @brief Default constructor.
+     */
+    Moderator();
+
+    /**
+     * @brief Clones the Moderator object.
+     *
+     * @return Pointer to the new User object (in this case Moderator).
+     */
+    virtual User* clone() const override;
+
+    /**
+     * @brief Saves moderator data to a file.
+     *
+     * @param of The output file stream.
+     */
+    virtual void writeToFile(std::ofstream& of) const override;
+
+    /**
+     * @brief Reads moderator data from a file.
+     *
+     * @param iff The input file stream.
+     */
+    virtual void readFromFile(std::ifstream& iff) override;
+};
